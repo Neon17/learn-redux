9f448e0a70fcf32ef1d83749e5460bde52d9bfff
Neon17 <neon.neupane.9@gmail.com>
2025-11-16 12:57:03 +0545

    setup: setup new vite react ts project and tailwind to start practicing the learned concepts in react with async data fetching

---
+++ practice-redux/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="btn p-0 m-0">
+        Hello
+      </div>
+    </>
+  )
+}
+
+export default App
